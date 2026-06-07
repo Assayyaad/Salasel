2a5e14f5008534be34931e650d0d743118510f4b
devAmjad4590 <developeramjad4590@gmail.com>
2026-06-07 10:10:21 +0300

    delete PersonalProgress and remove from playlist page

--- app/[lang]/playlist/[id]/page.tsx
+++ app/[lang]/playlist/[id]/page.tsx
@@ -3,7 +3,6 @@ import type { SelectedPlaylistParams } from '@/app/[lang]/playlist/[id]/params'
 import React from 'react'
 import SelectedPlaylistCard from '@/app/[lang]/playlist/[id]/components/SelectedPlaylistCard'
 import SelectedPlaylistContent from '@/app/[lang]/playlist/[id]/components/SelectedPlaylistContent'
-import PersonalProgress from '@/app/[lang]/playlist/[id]/components/PersonalProgress'
 import { getPlaylist, getVideos } from '@/app/db'
 import { getTranslations } from '@/app/translate'
 
@@ -34,7 +33,6 @@ const SelectedPlaylistPage: React.FC<SelectedPlaylistPageProps> = async ({ param
   return (
     <>
       <SelectedPlaylistCard playlist={playlist} t={t} />
-      <PersonalProgress playlist={playlist} videos={videos} t={t} />
       <SelectedPlaylistContent playlist={playlist} videos={videos} t={t} />
     </>
   )
